11996e9d54d1929c463f6f5be23b5e5f5445ccaa
mgindrat <michel.gindrat@yahoo.fr>
2023-09-13 20:59:39 +0200

    Scss (update)

--- p6/src/components/Collapse.jsx
+++ p6/src/components/Collapse.jsx
@@ -8,7 +8,7 @@ export default function Collapse({title, content}) {
     return (
         <>
             <div className="collapse" >
-                <h3 className='collapse_title' onClick={() => setToggle(!toggle)} >
+                <h3 className='collapse-title' onClick={() => setToggle(!toggle)} >
                     {title}
                     <img 
                         className={toggle ? 'arrow arrow_up' : 'arrow arrow_down'} 
@@ -16,7 +16,7 @@ export default function Collapse({title, content}) {
                         alt="show content" 
                     />
                 </h3>
-                <div className={toggle ? 'collapse_content' : 'collapse_content_hidden'}>
+                <div className={toggle ? 'collapse-content' : 'collapse-content-hidden'}>
                     {Array.isArray(content) ? content.map((item, index) => {
                         return (
                             <p key={index}>{item}</p>
